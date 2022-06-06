0c3dff4c24e7689c6d690aa4dbfe9ca9c96584f1
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2022-06-06 00:15:25 -0500

    Primer ejercicio finalizado

---
+++ act1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class act1 {
+    public static void main(String[] args) {
+        double n1,n2,s,r,d,m,rs;
+        Scanner Ejemplo1 = new Scanner(System.in);
+
+        System.out.println("Ingrese primer valor");
+        n1 = Ejemplo1.nextInt();
+
+        System.out.println("Ingrese segundo valor");
+        n2 = Ejemplo1.nextInt();
+
+        s=n1+n2;
+        r=n1-n2;
+        d=n1/n2;
+        m=n1*n2;
+        rs=n1%n2;
+
+        System.out.println("El resultado de la suma es " + s);
+        System.out.println("El resultado de la resta es " + r);
+        System.out.println("El resultado de la division es " + d);
+        System.out.println("El resultado de la multiplicacion es " + m);
+        System.out.println("El resultado del resto es " + rs);
+    }
+}
