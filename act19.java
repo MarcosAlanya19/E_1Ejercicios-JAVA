95dd214af215941530e730cf18d86d1829579915
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-06-08 01:44:04 -0500

    ADD: modificacion en  act19

--- act19.java
+++ act19.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class act19 {
@@ -8,18 +9,14 @@ public class act19 {
     // divisible desde ese numero hasta 1.
     //NOTA: Si se introduce un numero menor o igual que 1, directamente es no primo.
     public static void main(String[] args) {
-        Scanner ejm19=new Scanner(System.in);
-
-        System.out.println("Ingresa numero");
-        int numPrimo= ejm19.nextInt();
-
-        if(act19.esPrimo(numPrimo)){
-            System.out.println(numPrimo+" es primo");
-        }
-        else {
-            System.out.println(numPrimo+ " no es primo");
-        }
+        boolean retry = true;
+        do {
+            act19.esPrimoUser();
+            retry = act19.toTryAgain();
+        } while (retry);
+        System.out.println("FIN DEL PROGRAMA");
     }
+
     public static boolean esPrimo(int numero) {
         // El 0, 1 y 4 no son primos
         if (numero == 0 || numero == 1 || numero == 4) {
@@ -33,5 +30,24 @@ public class act19 {
         }
         // Si no se pudo dividir por ninguno de los de arriba, sí es primo
         return true;
+    }
+
+    public static void esPrimoUser() {
+        Scanner ejm19 = new Scanner(System.in);
+        System.out.println("Ingresa numero");
+        int numPrimo = ejm19.nextInt();
+        if (act19.esPrimo(numPrimo)) {
+            System.out.println(numPrimo + " es primo");
+        } else {
+            System.out.println(numPrimo + " no es primo");
+        }
+
+    }
+
+    public static boolean toTryAgain() {
+        System.out.println("¿Quieres volver a ingresar un número");
+        Scanner value = new Scanner(System.in);
+        String response = value.next();
+        return (response.toUpperCase().equals("SI"));
     }
 }
